890dca381c93ba6a8f819630487e730db89dbc5f
Rajan <kaushalrajan914@gmail.com>
2026-01-01 19:09:03 +0530

    this is my Leet codde problems with order wise

---
+++ Leetcode-problems/P_04_Remove_duplicatesFrom_sortedarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        k = 0  # index of last unique element
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k]:
+                k += 1
+                nums[k] = nums[i]
+        
+        return k + 1
+    
+    
+
+
+    
