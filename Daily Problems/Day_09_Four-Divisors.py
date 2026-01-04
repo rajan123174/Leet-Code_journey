13c759e14f499e918dbc91cb7ebb0451855d2d06
Rajan <kaushalrajan914@gmail.com>
2026-01-04 22:31:09 +0530

    This is my Daily Leet code problem Streak that is a medium level question

---
+++ Daily Problems/Day_09_Four-Divisors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sumFourDivisors(self, nums):
+        total_sum = 0
+
+        for n in nums:
+            divisors = set()
+            i = 1
+
+            while i * i <= n:
+                if n % i == 0:
+                    divisors.add(i)
+                    divisors.add(n // i)
+                    if len(divisors) > 4:
+                        break
+                i += 1
+
+            if len(divisors) == 4:
+                total_sum += sum(divisors)
+
+        return total_sum
