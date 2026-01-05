98097f24a37ae49a870128fc954101803aca4c7d
Rajan <kaushalrajan914@gmail.com>
2026-01-05 21:55:29 +0530

    This is my Day 09 Leet code problem that is ZIGZAG CONVERSION

---
+++ Leetcode-problems/P_09_Zigzag-Conversion.py
@@ -0,0 +1,20 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        current_row = 0
+        direction = 1  # 1 = down, -1 = up
+
+        for char in s:
+            rows[current_row] += char
+
+            if current_row == 0:
+                direction = 1
+            elif current_row == numRows - 1:
+                direction = -1
+
+            current_row += direction
+
+        return "".join(rows)
