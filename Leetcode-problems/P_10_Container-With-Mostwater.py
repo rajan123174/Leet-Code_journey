40ddf9158edf0ecb29f4257d5ac18cdc74dac4b0
Rajan <kaushalrajan914@gmail.com>
2026-01-13 15:23:48 +0530

    I have modified the code with a brute force and most optimal approach that is two pointers approach

--- Leetcode-problems/P_10_Container-With-Mostwater.py
+++ Leetcode-problems/P_10_Container-With-Mostwater.py
@@ -17,3 +17,44 @@ class Solution:
                 right -= 1
         
         return max_area
+    
+# Brute force approach 
+
+def maxWater(n,height):
+    ans = 0
+    for i in range(len(height)):
+        for j in range(i,len(height)):
+            width = j-i
+            hight = min(height[i],height[j])
+            area = width * hight
+        ans = max(ans,area)
+    return ans
+
+height = [1,8,6,2,5,4,8,3,7]
+n = len(height)
+print(maxWater(n,height))
+
+# Optimal approach i.e Two pointer approach 
+
+def optimalmaxWater(height,n):
+    maxWater = 0
+    rp = n-1
+    lp = 0
+
+    while lp < rp :
+        wd = rp - lp
+        ht = min(height[lp],height[rp])
+        currWater = wd * ht
+        maxWater = max(maxWater,currWater)
+
+        if height[lp] < height[rp]:
+            lp +=1
+        else:
+            rp -= 1
+    return maxWater
+
+print(optimalmaxWater(height,n))
+        
+
+
+
