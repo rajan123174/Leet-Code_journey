fbd3e7ed3ab8b51fe8d200b63f420d8e0f7616b2
Rajan <kaushalrajan914@gmail.com>
2026-01-14 16:16:01 +0530

    This is my Leet code problem 14 That is Search in a rotated array Based on binary search approach

---
+++ Leetcode-problems/Search-in-rotated_array.py
@@ -0,0 +1,29 @@
+def rotatedSortedArray(nums,target):
+    start = 0 
+    end = len(nums)
+
+    while start <= end:
+        mid = start + (end-start)//2
+        if nums[mid] == target:
+            return mid
+        
+        if nums[start] <= nums[mid]:# Left sorted array
+            if nums[start] <= target and target <= nums[mid]:# left half
+                end = mid-1
+            else:
+                start = mid+1 # right half
+
+        else: # right sorted array
+            if nums[mid] <= target and target <= nums[end]: # left half of right
+                start = mid+1 
+            else:
+                end = mid-1 # right
+    return -1
+    
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+print(rotatedSortedArray(nums,target))
+
+
+        
