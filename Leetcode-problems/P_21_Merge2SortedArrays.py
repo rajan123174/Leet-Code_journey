fb7431cabf64001b97fc0705fe14a72636ed3852
Rajan <kaushalrajan914@gmail.com>
2026-01-20 19:29:03 +0530

    this is my merge two sorted array Leet code question

--- Leetcode-problems/P_21_Merge2SortedArrays.py
+++ Leetcode-problems/P_21_Merge2SortedArrays.py
@@ -2,4 +2,28 @@
 A = [1,2,3,0,0,0], m = 3 B = [2,5,6], n= 3
 '''
 
+def mergeSortedArray(A,B,m,n):
+    idx, i, j = m+n-1, m-1, n-1
+
+    while i >= 0 and j>= 0:
+        if A[i] >= B[j]:
+            A[idx] = A[i]
+            idx -= 1
+            i -= 1
+        else:
+            A[idx] = B[j]
+            idx -= 1
+            j -= 1
+    while j >= 0:
+        A[idx] = B[j]
+        idx -= 1
+        j -= 1
+    return A
+
+
+A = [1,2,3,0,0,0] 
+B = [2,5,6]
+m = 3
+n = 3
+print(mergeSortedArray(A,B,m,n))
 
